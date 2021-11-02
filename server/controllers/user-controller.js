68e80323ea9b70dcc494c7982dc92fb4ff1efc39
hadalittlelamb <37488301+hLamb@users.noreply.github.com>
2021-11-02 12:56:36 -0400

    implemented logging in
    to clarify, a user may log in when the correct fields are applied.

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,17 +3,23 @@ const User = require('../models/user-model')
 const bcrypt = require('bcryptjs')
 
 getLoggedIn = async (req, res) => {
-    auth.verify(req, res, async function () {
-        const loggedInUser = await User.findOne({ _id: req.userId });
-        return res.status(200).json({
-            loggedIn: true,
-            user: {
-                firstName: loggedInUser.firstName,
-                lastName: loggedInUser.lastName,
-                email: loggedInUser.email
-            }
-        }).send();
-    })
+    try{
+        auth.verify(req, res, async function () {
+            const loggedInUser = await User.findOne({ _id: req.userId });
+            return res.status(200).json({
+                loggedIn: true,
+                user: {
+                    firstName: loggedInUser.firstName,
+                    lastName: loggedInUser.lastName,
+                    email: loggedInUser.email
+                }
+            }).send();
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+    
 }
 
 registerUser = async (req, res) => {
@@ -78,7 +84,60 @@ registerUser = async (req, res) => {
     }
 }
 
+loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
+        }
+        if (password.length < 8) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter a password of at least 8 characters."
+                });
+        }
+        const existingUser = await User.findOne({ email: email });
+        if (!existingUser) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "Invalid email and/or password."
+                })
+        }
+
+        bcrypt.compare(password, existingUser.passwordHash, async function(err,same) {
+            if(err)
+                console.log(err);
+            if(same) {
+                // LOGIN THE USER
+                const token = auth.signToken(existingUser);
+
+                await res.cookie("token", token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "none"
+                }).status(200).json({
+                    success: true,
+                    user: {
+                        firstName: existingUser.firstName,
+                        lastName: existingUser.lastName,
+                        email: existingUser.email
+                    }
+                }).send();
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
 module.exports = {
     getLoggedIn,
-    registerUser
+    registerUser,
+    loginUser
 }
